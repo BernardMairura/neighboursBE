8446c9eec6d32f3476cc65901676d448682e8ddd
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-12-03 09:17:14 +0300

    changes on custom user

--- neighbours/settings.py
+++ neighbours/settings.py
@@ -40,6 +40,10 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework.authtoken',
+    'rest_framework',
+    'cloudinary',
+    'neighboursapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,6 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -71,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
--- neighboursapp/models.py
+++ neighboursapp/models.py
@@ -1,54 +1,193 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,User
 from django.db import models
 from django.urls import reverse
+from django.db.models import CharField
+from cloudinary.models import CloudinaryField
+from tinymce.models import HTMLField
 from django.utils.translation import gettext_lazy as _
 
 
 
-class User(AbstractUser):
-    class Types(models.TextChoices):
-        SYSTEMADMIN="SYSTEMADMIN","SystemAdmin"
-        NEIGHBORADMIN="NEIGHBORADMIN","NeighborAdmin"
-        NEIGHBOR="NEIGHBOR","Neighbor"
+# class User(AbstractUser):
 
-        type=models.CharField(_('Type')max_length=50, choices=Types.choices,default=Types.NEIGHBOR)
+#     is_neighbor=models.BooleanField(default=True)
 
 
-        name=models.CharField(_("Name of User"),blank=True,max_length=255)
 
-        def get_absolute_url(self):
-            return reverse("users:detail",kwargs={"username":self.username})
-
-
-class SytemadminManager(models.Manager):
-    def get_queryset(self, *args, **kwargs):
-        return super().get_queryset(*args, **kwargs).filter(type=User.Types.SYSTEMADMIN)
-
-class NeighboradminManager(models.Manager):
-    def get_queryset(self, *args, **kwargs):
-        return super().get_queryset(*args, **kwargs).filter(type=User.Types.NEIGHBORADMIN)
+class NeighborProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE, null=True, related_name='neighbor_profile')
+    location=models.CharField(max_length=30,blank=True)
+    bio =HTMLField(max_length=100, blank=True)
+    prof_picture= CloudinaryField('image')
+    contact = models.CharField(max_length=15, blank=True)
+    hoodname = models.ForeignKey("Neighborhood", related_name='home', null=True)
 
 
-class NeighborManager(models.Manager):
-    def get_queryset(self, *args, **kwargs):
-        return super().get_queryset(*args, **kwargs).filter(type=User.Types.NEIGHBOR)
 
+class HoodadminProfile(models.Model):
+    admin = models.ForeignKey(User, related_name='hood_administrator')
+    bio = HTMLField(max_length=100, blank=True)
+    prof_picture= CloudinaryField('image')
 
-class SystemAdmin(User):
-    objects=SystemAdmin
+     def __str__(self):
+        return f'{self.user.username} HoodadminProfile('
+
     class Meta:
-        proxy=True
+        db_table = 'adminprof'
+ 
+   
 
 
-class NeighborAdmin(User):
-    objects=NeighborManager
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def __str__(self):
+        return self.user.username
+
+
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey("HoodadminProfile", on_delete=models.CASCADE, related_name='neighborhood')
+    hoodphoto = CloudinaryField('image')
+    body= HTMLField()
+    residents= models.IntegerField(null=True, blank=True)
+    emergency_contact = models.IntegerField(null=True, blank=True)
+
     class Meta:
-        proxy=True
+        db_table = 'neighborhood'
+    
 
-class Neighbor(User):
-    objects=Neighbor
-    class Meta:
-        proxy=True
+    def __str__(self):
+        return f'{self.name} neighborhood'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
+
+
+   
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField(max_length=254)
+    body = HTMLField(blank=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='business')
+    location = models.CharField(max_length=60)
+
+    def __str__(self):
+        return f'{self.name} Business'
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    @classmethod
+    def search_business(cls, name):
+        return cls.objects.filter(name__icontains=name).all()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class User(AbstractUser):
+#     class Types(models.TextChoices):
+#         SYSTEMADMIN="SYSTEMADMIN","SystemAdmin"
+#         NEIGHBORADMIN="NEIGHBORADMIN","NeighborAdmin"
+#         NEIGHBOR="NEIGHBOR","Neighbor"
+
+#         type=models.CharField(_('Type'),max_length=50, choices=Types.choices,default=Types.NEIGHBOR)
+
+
+#         name=models.CharField(_("Name of User"),blank=True,max_length=255)
+
+#         def get_absolute_url(self):
+#             return reverse("users:detail",kwargs={"username":self.username})
+
+
+# class SytemadminManager(models.Manager):
+#     def get_queryset(self, *args, **kwargs):
+#         return super().get_queryset(*args, **kwargs).filter(type=User.Types.SYSTEMADMIN)
+
+# class NeighboradminManager(models.Manager):
+#     def get_queryset(self, *args, **kwargs):
+#         return super().get_queryset(*args, **kwargs).filter(type=User.Types.NEIGHBORADMIN)
+
+
+# class NeighborManager(models.Manager):
+#     def get_queryset(self, *args, **kwargs):
+#         return super().get_queryset(*args, **kwargs).filter(type=User.Types.NEIGHBOR)
+
+
+# class SystemAdmin(User):
+#     objects=SystemAdmin
+#     class Meta:
+#         proxy=True
+
+
+# class NeighborAdmin(User):
+#     objects=NeighborManager
+#     class Meta:
+#         proxy=True
+
+# class Neighbor(User):
+#     objects=Neighbor
+#     class Meta:
+#         proxy=True
 
 
 
